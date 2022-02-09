93203cdf68f810e961db31a3f3adebd5c589fcf8
alexia19 <1907alexiaau@gmail.com>
2022-02-09 20:01:31 +0100

    comienzo del proyecto

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php 
+ echo '<h1>BIENVENID@</h1>'; 
+ echo '<p>Alexia Alobuela Ushiña - Despliegue</p>'
+ ?>
+ </body>
+</html>
